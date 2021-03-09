1d579615ec8aa4ad3fd201e144cfff5f128fccee
okikiola11 <okikiola.apelehin@gmail.com>
2021-03-09 23:34:14 +0000

    Wirte logic to add products to loaded static content

---
+++ app.js
@@ -0,0 +1,45 @@
+// Declare variables
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const caetDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+
+const prouctsDOM = document.querySelector('.product-center');
+
+// cart
+let cart = [];
+
+//getting the products
+class Products {
+  async getProducts() {
+    try {
+      let response = await fetch('products.json');
+      let data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+} 
+
+// display products
+class UI {
+
+}
+
+// Local storage
+class Storage {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI();
+  const products = new Products();
+  
+  //get all products
+  products.getProducts().then(data => console.log(data))
+})
