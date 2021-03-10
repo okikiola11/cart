cc45e71e4595c491bb63af90875e0d895a56bfe0
okikiola11 <okikiola.apelehin@gmail.com>
2021-03-10 08:46:27 +0000

    Display products details from the json file Extract data using the forEach method on each product data

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 
-const prouctsDOM = document.querySelector('.product-center');
+const productsDOM = document.querySelector('.products-center');
 
 // cart
 let cart = [];
@@ -35,7 +35,25 @@ class Products {
 
 // display products
 class UI {
-
+  displayProducts(products) {
+   let result = '';
+   products.forEach(product => {
+     result += `
+        <article class="product">
+          <div class="img-container">
+            <img src=${product.image} alt="product" class="product-img">
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+     `;
+   });
+   productsDOM.innerHTML = result;
+  }
 }
 
 // Local storage
@@ -48,5 +66,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const products = new Products();
   
   //get all products
-  products.getProducts().then(data => console.log(data))
+  products.getProducts().then(products => ui.displayProducts(products))
 })
