417ceaf157bb8372c7ee25e6d9a8bc94c4b9c873
okikiola11 <okikiola.apelehin@gmail.com>
2021-03-10 08:07:17 +0000

    Fetch data from the product.json file

--- app.js
+++ app.js
@@ -19,9 +19,16 @@ class Products {
     try {
       let response = await fetch('products.json');
       let data = await response.json();
-      return data;
+      let products = data.items;
+      products = products.map(item => {
+        const {title, price} = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      })
+      return products;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 } 
